3a97f17fe4a4367781ab57826d5744564af013ee
Connor F <connorfinnigan@gmail.com>
2019-04-09 23:14:03 -0500

    updates to server file and package.json

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const http = require("http");
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
 
-const PORT = 3000;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
 
-function handleRequest(request, response){
-    response.end("Cool it works! Path hit here:" + request.url);
-}
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var server = http.createServer(handleRequest);
+// function handleRequest(request, response){
+//     response.end("Cool it works! Path hit here:" + request.url);
+// }
 
-server.listen(PORT, function(){
+// var server = http.createServer(handleRequest);
+
+app.listen(PORT, function(){
     console.log("server up! on http://localhost:" + PORT);
 })
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
